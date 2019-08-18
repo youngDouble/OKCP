1d663c2aa6beba9942f734d02b87a8c4f5cdf6f4
ZhiShuang Yang <yangzs_chi@yeah.net>
2019-08-18 14:37:39 +0800

    Update gbk2faa.py

--- bin/gbk2faa.py
+++ bin/gbk2faa.py
@@ -4,9 +4,10 @@
 ####################################################################################################
 Author:yangzhushuang
 E-mail:yangzs_chi@yeah.net
-Edited by Yang Zhishuang at 2018/01/18
-The latest version was edited at : 2018/01/18 By:yangzs
+Edited by Yang Zhishuang at 2019/08/18
+The latest version was edited at : 2019/08/18 By:yangzs
 ####################################################################################################
+
 Description:
 
     This shellscript  is used to convert genbank to faa.
@@ -26,17 +27,24 @@ def gb2faa(gbk_filename, faa_filename):
     input_handle = open(gbk_filename, "r")
     output_handle = open(faa_filename, "w")
     for seq_record in SeqIO.parse(input_handle, "genbank"):
-        #print "Dealing with GenBank record %s" % seq_record.id
+        print "Dealing with GenBank record %s" % seq_record.id
         for seq_feature in seq_record.features:
-            if seq_feature.type == "CDS":
-                assert len(seq_feature.qualifiers['translation']) == 1
-                output_handle.write(">%s from %s\n%s\n" % (
-                    seq_feature.qualifiers['locus_tag'][0],
-                    seq_record.name,
-                    seq_feature.qualifiers['translation'][0]))
+            try:  # Without "try", it crashes when it finds a CDS without translation (pseudogene).
+                if seq_feature.type == "CDS":
+                    assert len(seq_feature.qualifiers['translation']) == 1
+                    output_handle.write(">%s %s %s %s\n%s\n" % (
+                        seq_feature.qualifiers['locus_tag'][0],
+                        seq_feature.qualifiers['product'][0],
+                        seq_record.id,
+                        seq_record.description,
+                        seq_feature.qualifiers['translation'][0]))
+                    pass
+            except:
+                continue
 
     output_handle.close()
     input_handle.close()
+
 
 if __name__=='__main__':
     try:
