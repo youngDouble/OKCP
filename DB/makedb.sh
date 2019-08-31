b114bb0e28053969559b1c2089a4ac332d752072
ZhiShuang Yang <yangzs_chi@yeah.net>
2019-08-31 15:41:28 +0800

    Delete makedb.sh

--- DB/makedb.sh
+++
@@ -1,7 +0,0 @@
-#cp O-antigen_DB_from_NCBI_fasta_cds_aa.faa O-antigen_DB_from_NCBI_fasta_cds_aa.faa.b
-perl -p -i -e 's/\>.+?\[gene=/\>/;s/\[/ /g;s/\]//g;s/lcl\|.+?_prot_(.+?) /$1/'  O-antigen_DB_from_NCBI_fasta_cds_aa.faa
-makeblastdb  -in O-antigen_DB_from_NCBI_fasta_cds_aa.faa   -input_type fasta -dbtype prot  -out O-antigen_DB_from_NCBI_fasta_cds_aa
-
-perl -p -i -e 's/\>.+?\[gene=/\>/;s/\[/ /g;s/\]//g;s/lcl\|.+?_prot_(.+?) /$1/'   K-antigen_DB_from_NCBI_fasta_cds_aa.faa
-makeblastdb  -in K-antigen_DB_from_NCBI_fasta_cds_aa.faa  -input_type fasta -dbtype prot  -out K-antigen_DB_from_NCBI_fasta_cds_aa
-
